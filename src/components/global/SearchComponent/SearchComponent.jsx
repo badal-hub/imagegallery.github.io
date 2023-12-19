495cc7e7d9c8a9f44379845d9e5924254f9153aa
Badal Prasad Mehta <65506036+badal-hub@users.noreply.github.com>
2023-12-20 01:00:30 +0530

    first

---
+++ src/components/global/SearchComponent/SearchComponent.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react';
+import "./SearchComponent.scss"
+const SearchComponent = () => {
+  return (
+   <Box className="searchComponentBlock">
+    search will be going  here
+   </Box>
+  )
+}
+
+export default SearchComponent
---
+++ src/components/reusable/AppName/AppName.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react';
+import "./AppName.scss"
+Box
+const AppName = () => {
+  return (
+    <Box className="AppNameBlock">
+        <Box>
+            <Typography variant="h3">Image Gallery App using React</Typography>
+        </Box>
+    </Box>
+  )
+}
+
+export default AppName
